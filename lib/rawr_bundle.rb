a00ab82a572e804f02e59bfd152a65e7b11348e2
James Britt <james.britt@gmail.com>
2008-03-09 16:52:34 -0700

    Change File.copy to FileUtils.copy

--- lib/rawr_bundle.rb
+++ lib/rawr_bundle.rb
@@ -155,6 +155,6 @@ def copy_deployment_to(destination_path)
   relative_package_dir = PACKAGE_DIR.gsub("#{BASE_DIR}/", '')
   (Dir.glob("#{PACKAGE_DIR}/**/*").reject{|e| e =~ /\.svn/}.map{|file| file.gsub(BASE_DIR + '/', '')} + CLASSPATH_FILES).flatten.uniq.each do |file|
      FileUtils.mkdir_p("#{destination_path}/#{File.dirname(file).gsub(relative_package_dir, '')}")
-     File.copy(file, "#{destination_path}/#{file.gsub(relative_package_dir, '')}") unless File.directory?(file)
+     FileUtils.copy(file, "#{destination_path}/#{file.gsub(relative_package_dir, '')}") unless File.directory?(file)
   end
 end
