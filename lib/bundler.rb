d8ec2cc6eab9ea04a55755f59851eeb999beed2a
Logan Barnett <logustus@gmail.com>
2008-06-05 15:16:45 -0700

    Fixed classpath directories. They are relative now. This helps fix issue #23 and #5.

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -2,20 +2,30 @@ require 'fileutils'
 
 module Rawr
   class Bundler
+    #TODO: Remove in favor of file_utils method. Check other bundlers
     include FileUtils
 
     def copy_deployment_to(destination_path)
-      mkdir_p destination_path
+      file_utils.mkdir_p destination_path
       
-      relative_package_dir = @package_dir.gsub("#{@base_dir}/", '')
+      relative_base_dir = @package_dir.sub("#{@base_dir}/", '')
       
       files_without_repo = Dir.glob("#{@package_dir}/**/*").reject{|e| e =~ /\.svn/}
-      relative_files_without_repo = files_without_repo.map{|file| file.gsub(@base_dir + '/', '')}
+      relative_files_without_repo = files_without_repo.map{|file| file.sub(@base_dir + '/', '')}
       
-      (relative_files_without_repo + @classpath).flatten.uniq.each do |file|
-         FileUtils.mkdir_p("#{destination_path}/#{File.dirname(file).gsub(relative_package_dir, '')}")
-         FileUtils.copy(file, "#{destination_path}/#{file.gsub(relative_package_dir, '')}") unless File.directory?(file)
+      (relative_files_without_repo + relative_classpath).flatten.uniq.each do |file|
+         file_utils.mkdir_p("#{destination_path}/#{File.dirname(file).sub(relative_base_dir, '')}")
+         file_utils.copy(file, "#{destination_path}/#{file.sub(relative_base_dir, '')}") unless File.directory?(file)
       end
+    end
+    
+    private
+    def relative_classpath
+      @classpath.map {|file| file.sub("#{@base_dir}/", '')}
+    end
+    
+    def file_utils
+      FileUtils
     end
   end
 end
