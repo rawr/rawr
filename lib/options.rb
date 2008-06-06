bd28b8f3c58871866a98b282bf5c8497a15829fe
James Britt <james@happycamperstudios.com>
2008-06-05 18:26:12 -0700

    Code will now see if there is a web_start section in the options set, and see if it can use self-signing configuration

--- lib/options.rb
+++ lib/options.rb
@@ -5,40 +5,52 @@ require 'jar_builder'
 module Rawr
   class Options
     include Singleton
-    
+
     def self.[](key)
       self.instance[key]
     end
-    
+
     def self.load_configuration(file)
       self.instance.load_configuration file
     end
-    
-    
+
+
     def [](key)
       @options[key]
     end
-    
-    def load_configuration(file='build_configuration.yaml')
-      @options = {}
-      
+
+    def load_configuration(file='build_configuration.yaml') 
       begin
         @config = YAML.load_file file
       rescue Errno::ENOENT
         @config = {}
       end
+      process_configuration @config
+    end
 
-      @options[:project_name] = @config['project_name'] || "JRuby project"
+
+    def process_configuration(config)
+      @options = {}
+
+      @options[:project_name] = config['project_name'] || "JRuby project"
       @options[:base_dir] = Dir::pwd
-      @options[:output_dir] = "#{@options[:base_dir]}/#{@config['output_dir']}" || "#{@options[:base_dir]}/package"
+      @options[:output_dir] = "#{@options[:base_dir]}/#{config['output_dir']}" || "#{@options[:base_dir]}/package"
       @options[:build_dir] = "#{@options[:output_dir]}/bin"
       @options[:package_dir] = "#{@options[:output_dir]}/deploy"
-      
-      load_ruby_options(@config)
-      load_java_options(@config)
-      load_jars_options(@config)
+
+      load_ruby_options(config)
+      load_java_options(config)
+      load_jars_options(config)
+      load_web_start_options(config)
     end
-    
+
+    def  load_web_start_options(config_hash)
+      @options ||= {}
+      @options[:web_start] = config_hash['web_start'] 
+      @options[:web_start][:self_sign] =  config_hash['web_start']['self_sign'] 
+      @options[:web_start][:self_sign_passphrase] =  config_hash['web_start']['self_sign_passphrase'] 
+    end
+
     def load_ruby_options(config_hash)
       @options ||= {}
       @options[:ruby_source_dir] = "#{@options[:base_dir]}/#{config_hash['ruby_source_dir']}" || "#{@options[:base_dir]}/src"
@@ -47,7 +59,7 @@ module Rawr
       @options[:ruby_library] = config_hash['ruby_library_dir'] || "lib"
       @options[:main_ruby_file] = config_hash['main_ruby_file'] || "main"
     end
-    
+
     def load_java_options(config_hash)
       @options ||= {}
       @options[:main_java_file] = config_hash['main_java_file'] || "org.rubyforge.rawr.Main"
@@ -59,7 +71,7 @@ module Rawr
       @options[:jar_data_dirs] = config_hash['jar_data_dirs'] || []
       @options[:package_data_dirs] = config_hash['package_data_dirs'] || []
     end
-    
+
     def load_jars_options(config_hash)
       @options ||= {}
       @options[:classpath] ||= []
@@ -68,19 +80,19 @@ module Rawr
       jars_hash.each do |name, jar_options|
         jar_dir = jar_options['dir'] || '/'
         jar_dirs = [jar_dir] unless jar_dir.kind_of? Array
-        
+
         jar_glob = jar_options['glob'] || '**/*'
         jar_globs = [jar_glob] unless jar_glob.kind_of? Array
-        
+
         jar_builder = JarBuilder.new
         jar_builder.name = name
         jar_builder.dirs = jar_dirs
         jar_builder.globs = jar_globs
-        
+
         @options[:jars][name] = jar_builder
         @options[:classpath] << jar_builder.name
       end
     end
-      
+
   end
 end
