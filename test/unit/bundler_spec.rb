d22a99e2ce04c9dd6eb318fc3ec0321af9a6b92a
James Britt <james@happycamperstudios.com>
2008-06-05 22:24:50 -0700

    adjusting code and specs while getting web bundler code to correctly fix jar paths for jnlp packaging

--- test/unit/bundler_spec.rb
+++ test/unit/bundler_spec.rb
@@ -5,22 +5,23 @@ describe Rawr::Bundler do
   before :each do
     @bundler = Rawr::Bundler.new
   end
-  
+
   it "converts the classpath into a relative classpath" do
     @bundler.instance_variable_set(:@base_dir, "/root")
     @bundler.instance_variable_set(:@classpath, ["/root/foo.rb", "/root/bar.rb"])
-    
+
     @bundler.send(:relative_classpath).should == ["foo.rb", "bar.rb"]
   end
-  
+
   it "uses the relative classpath during the deployment copy" do
     @bundler.should_receive(:relative_classpath).and_return([])
-    
+
     @bundler.instance_variable_set(:@base_dir, "/root")
     @bundler.instance_variable_set(:@package_dir, "/root/package")
     @bundler.instance_variable_set(:@classpath, ["/root/foo.rb", "/root/bar.rb"])
     @bundler.stub!(:file_utils).and_return(FileUtils::DryRun)
-    
+
     @bundler.copy_deployment_to "temp_deploy"
   end
+
 end
