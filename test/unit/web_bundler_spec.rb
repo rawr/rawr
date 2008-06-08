bb8bc2dd9ef9598273071f6910549c794e48821f
James Britt <james@happycamperstudios.com>
2008-06-08 16:42:00 -0700

    Fixed bug introduced into options class, where it would error if there was no jnlp section in build_configuration

--- test/unit/web_bundler_spec.rb
+++ test/unit/web_bundler_spec.rb
@@ -13,16 +13,23 @@ describe Rawr::WebBundler do
   end
 
   it "adjusts the path of jars in the classpath " do
-    #    @web_bundler.instance_variable_set(:@base_dir, "/path/to/project/root")
-    #    @web_bundler.instance_variable_set(:@output_dir, "/path/to/project/root/package")
-    #    @web_bundler.instance_variable_set(:@package_dir, "/path/to/project/root/package/deploy")
+    @web_bundler.instance_variable_set(:@base_dir, "/path/to/project/root")
+    @web_bundler.instance_variable_set(:@output_dir, "/path/to/project/root/package")
+    @web_bundler.instance_variable_set(:@package_dir, "/path/to/project/root/package/deploy")
 
-    #    @web_bundler.instance_variable_set(:@classpath, ["/root/foo.rb", "/root/bar.rb"])
+    @web_bundler.instance_variable_set(:@classpath, ["/root/foo.rb", "/root/bar.rb"])
 
-    #    path1= 'lib/java/derby-10.3.2.1.jar'
-    #    @web_bundler.to_web_path( path1 ).should ==  '/path/to/project/root/package/native_deploy/web/lib/java/derby-10.3.2.1.jar'
-    #    path1= '/path/to/project/root/package/deploy/foo.jar'
-    #    @web_bundler.to_web_path( path1 ).should ==  '/path/to/project/root/package/native_deploy/web/foo.jar'
+    path1= 'lib/java/derby-10.3.2.1.jar'
+    @web_bundler.to_web_path( path1 ).should ==  '/path/to/project/root/package/native_deploy/web/lib/java/derby-10.3.2.1.jar'
+    path1= '/path/to/project/root/package/deploy/foo.jar'
+    @web_bundler.to_web_path( path1 ).should ==  '/path/to/project/root/package/native_deploy/web/foo.jar'
+  end
+
+
+
+  it "creates a JNLP string from the web_start JNLP configuration values" do
+    config = YAML.load(BUILD_CONFIGURATION )
+
   end
 
 
