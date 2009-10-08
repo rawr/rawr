1d0efd9bcc265384fabee3437d98c2ec61998482
Logan Barnett <logustus@gmail.com>
2009-10-08 14:57:42 -0700

    Progress on shell bundler.

--- lib/shell_bundler.rb
+++ lib/shell_bundler.rb
@@ -18,13 +18,25 @@ module Rawr
 
     def create_shell_file(project_name)
       shell_file_name = project_name + '.sh'
-      File.open(shell_file_name,'w') do |f|
+      File.open(shell_file_name, 'w') do |f|
         f << <<-ENDL
 #! /bin/sh
-java -jar #{project_name}.jar
+java #{jvm_arguments} #{native_library_path} -jar #{project_name}.jar
 ENDL
       end
-      chmod 0755, shell_file_name
+      chmod 0755, File.expand_path(shell_file_name)
+    end
+
+    def jvm_arguments
+      ''
+    end
+
+    def native_library_path
+      if false
+        "-Djava.library.path=#{path}"
+      else
+        ''
+      end
     end
   end
 end
